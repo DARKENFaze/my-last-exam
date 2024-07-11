80d95508462ff560944922fa89c23aa4424412e5
DARKEN-Ae <arslantrad897@gmail.com>
2024-07-11 13:51:53 +0500

    edit index

--- js/main.js
+++ js/main.js
@@ -29,5 +29,32 @@ $("#kurorty.owl-carousel").owlCarousel({
   },
 });
 
+//video
 
-// 
+const player = document.querySelector("#play-video");
+const videoOverlay = document.querySelector("#video-overlay");
+
+player.addEventListener("click", function (e) {
+  e.preventDefault();
+  videoOverlay.classList.add("open");
+  videoOverlay.innerHTML +=
+    '<iframe width="120" height="150" src="https://www.youtube.com/embed/vp2yiZnjK0w?si=WBnKMkhnL2vecmk8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+});
+
+videoOverlay.addEventListener("click", function (e) {
+  e.preventDefault();
+  close_video();
+});
+
+document.addEventListener("keyup", function (e) {
+  if (e.keyCode === 27) {
+    close_video();
+  }
+});
+
+function close_video() {
+  videoOverlay.classList.remove("open");
+  videoOverlay.querySelector("iframe").remove();
+}
+
+//  slider
